65fcdc52a046c2548bee50bfbbf766917b20c5bb
chenghaoqian <chenghaoqian@jimistore.com>
2020-01-19 20:56:53 +0800

    修改启动类

--- src/main/java/com/anhaoyang/test/TestApplication.java
+++ src/main/java/com/anhaoyang/test/TestApplication.java
@@ -2,12 +2,19 @@ package com.anhaoyang.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TestApplication {
+public class TestApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(TestApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(TestApplication.class);
     }
 
 }
